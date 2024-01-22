acf18d5de30ee15dc662250c617b1a4c0a9362e3
Acer <regina-sabitova-1998@mail.ru>
2024-01-22 23:02:45 +0500

    changed js

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
-let a = 20;
-alert(a);
+let password = "skypro";
+let enterPassword = prompt("Введите пароль");
+if (enterPassword === password) {
+  alert("Пароль введен верно");
+} else {
+  alert("Пароль введен неправильно");
+}
 
-let year = 2007;
-alert(year);
+let m = prompt(10);
+if (m > 0 && m < 10) {
+  alert("Верно");
+} else {
+  alert("Неверно");
+}
 
-let Name = "Brendan Eich";
-alert(Name);
+let number = Number (prompt('Введите число'));
+let h = 120;
+let e = 80;
 
-let b = 10;
-let c = 2;
-alert(b + c);
-alert(b - c);
-alert(b * c);
-alert(b / c);
+if (h > 100 || e > 100) {
+  alert("Верно");
+} else {
+  alert("Неверно");
+}
 
-let d = 2 ** 5;
-let result = 32;
-alert(result);
+let a = '2';
+let b = '3';
+a = Number(a);
+b = Number(b);
+alert(a+b);
 
-let i = 9;
-let f = 2;
-alert(i % f);
+let monthNumber = Number( prompt('Введите номер месяца'));
+switch (monthNumber) {
+  case 12:
+  case 1:
+  case 2:
+    alert('зима');
+    break;
+  case 3:
+  case 4:
+  case 5:
+    alert('весна');
+    break;
+  case 6:
+  case 7:
+  case 8:
+    alert('лето');
+    break;
+  case 9:
+  case 10:
+  case 11:
+    alert('осень');
+    break;
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
-
-let age = prompt("Сколько вам лет?");
-alert(age);
-
-let user = {
-  name: "nina",
-  age: 26,
-  isAdmin: true,
-};
-user.cityOfResidence = "Tula";
-user.age = 28;
-delete user.cityOfResidence;
-
-let info = prompt("Какую информацию хотите узнать о пользователе?");
-alert(user[info]);
-
-let youName = prompt("Ваше имя");
-alert("Привет," + youName + "!");
+  default:
+    alert('monthNumber' > 13);
+    break;
+}
